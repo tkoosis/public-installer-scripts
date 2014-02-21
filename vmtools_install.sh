e8b50ecdf2951b939be9a1b1acdf51c9917e989f
Telly Koosis <tkoosis@users.noreply.github.com>
2014-02-22 01:15:31 +1100

    Create vmtools_install.sh

---
+++ vmtools_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# sudo mkdir /mnt/cdrom
+# sudo mount /dev/cdrom /mnt/cdrom or sudo mount /dev/sr0 /mnt/cdrom
+# tar xzvf /mnt/cdrom/VMwareTools-x.x.x-xxxx.tar.gz -C /tmp/
+# cd /tmp/vmware-tools-distrib/
+# sudo ./vmware-install.pl -d
+# sudo reboot
+
+
+
+# ls /mnt/cdrom  > find exact name of bundle
+# + x.x.x-xxxx is the bundle version
